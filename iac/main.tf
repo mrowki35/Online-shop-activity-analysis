337249a7363511f9d09bc7c8bd6c5abf42a3c800
ThePolishGuy7 <mateusz.bugdol.s@gmail.com>
2026-01-11 13:06:52 +0100

    terraform

--- iac/main.tf
+++ iac/main.tf
@@ -1,35 +1,21 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "rq-clickstream-kappa-dev"
-  location = "Switzerland North"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "random_string" "random" {
-  length  = 8
+  length  = 6
   special = false
   upper   = false
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "rqclickstream${random_string.random.result}" 
+  name                     = "stclickstream${random_string.random.result}" 
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-
-  is_hns_enabled           = true 
+  is_hns_enabled           = true
 }
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "dl_fs" {
@@ -38,11 +24,11 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "dl_fs" {
 }
 
 resource "azurerm_eventhub_namespace" "eh_ns" {
-  name                = "eh-clickstream-${random_string.random.result}"
+  name                = "eh-ns-${random_string.random.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Basic" 
-  capacity            = 1      
+  sku                 = "Standard"
+  capacity            = 1
 }
 
 resource "azurerm_eventhub" "eh" {
@@ -54,7 +40,7 @@ resource "azurerm_eventhub" "eh" {
 }
 
 resource "azurerm_databricks_workspace" "db" {
-  name                = "db-clickstream-${random_string.random.result}"
+  name                = "db-workspace-${random_string.random.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "standard"
