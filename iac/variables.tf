668e17c645604357419de9bc9eab9ca94be9f77d
mrowki35 <orprzemek@gmail.com>
2026-01-11 13:17:51 +0100

    Update variables.tf

--- iac/variables.tf
+++ iac/variables.tf
@@ -11,5 +11,5 @@ variable "location" {
 variable "eh_conn_string" {
   description = "Connection string do Event Hub (dla Databricks)"
   type        = string
-  sensitive   = true # Terraform ukryje to w logach
+  sensitive   = true
 }
